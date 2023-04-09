e7c03ff1379d6cb1f14876ded36724cbfab23b56
Carmen Vargas <trishiavargas@gmail.com>
2023-04-09 14:54:32 -0300

    nuevos datos

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,24 +1,45 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 
-const Card = ({ name, username, id }) => {
+const Card = ({ name, username, id, showButton}) => {
+  const navigate = useNavigate();
 
   const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
-  }
+    let value: [] = [];
+    if (sessionStorage.getItem("token")) {
+      value = JSON.parse(sessionStorage.getItem("token"));
+      value.push({ id, name, username });
+      sessionStorage.setItem("token", JSON.stringify(value));
+      console.log(value);
+    } else {
+      sessionStorage.setItem("token", JSON.stringify([{ id, name, username }]));
+    }
+  }; // Aqui iria la logica para agregar la Card en el localStorage
+  const selectItem = () => {
+    navigate(`/home/detail/${id}`);
+  };
 
   return (
     <div className="card">
+      <div
+        onClick={selectItem}>
+          <img src="./images/doctor.jpg" 
+          alt="DH-logo" />
+      
         {/* En cada card deberan mostrar en name - username y el id */}
       <h3>{name}</h3>
       <h3>{username}</h3>
       <p>{id}</p>
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">
+        </div>
+        {showButton && (
+          <button onClick={addFav} className="favButton">
           Add fav
           </button>
+        )}
+        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+        
     </div>
   );
 };
