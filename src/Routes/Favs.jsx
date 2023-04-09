192e674c68cae741d5a5d92292a7addc29fbddd2
Carmen Vargas <trishiavargas@gmail.com>
2023-04-09 20:07:26 -0300

    final final

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -4,6 +4,7 @@ import Card from "../Components/Card";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
+  
 
   return (
     <>
